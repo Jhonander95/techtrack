5dece8176addd303c609f62540b73fd0a9d1109d
jhonander95 <tellezjhonander@gmail.com>
2024-12-17 07:15:52 -0500

    Feat: Add Firebase and PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnimationsAsync(), // Provee animaciones de Angular necesarias para PrimeNG
+    providePrimeNG({ 
+      theme: { 
+        preset: Aura // Define el tema de PrimeNG (Aura en este caso)
+      }
+    })
+  ]
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDvAMxwsMEzIaf766hHcyfBApZ9yq2HFFo",
+    authDomain: "techtrack-e15e0.firebaseapp.com",
+    projectId: "techtrack-e15e0",
+    storageBucket: "techtrack-e15e0.firebasestorage.app",
+    messagingSenderId: "621583022657",
+    appId: "1:621583022657:web:98d0843faa707fc159e4c0"
+  }
+}; 
