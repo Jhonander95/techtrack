b1db91c881a5ab970fb1f51b5fd85eb74bf8005f
jhonander95 <tellezjhonander@gmail.com>
2024-12-19 07:42:34 -0500

    Fix bug in firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
     provideAnimationsAsync(), // Provee animaciones de Angular necesarias para PrimeNG
     providePrimeNG({ 
       theme: { 
