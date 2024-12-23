229189859046d5ecce24c692b57b8e52ef7ee195
jhonander95 <tellezjhonander@gmail.com>
2024-12-23 07:42:54 -0500

    Implement grafics

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { jqxChartModule } from 'jqwidgets-ng/jqxchart'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ jqxChartModule ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+   // Propiedades del gráfico
+   data!: any[];
+   xAxis: any;
+   valueAxis: any;
+   seriesGroups!: any[];
+
+   constructor() {}
+
+  ngOnInit(): void {
+    // Datos de ejemplo
+    this.data = [
+      { day: 'Lunes', orders: 5 },
+      { day: 'Martes', orders: 10 },
+      { day: 'Miércoles', orders: 8 },
+      { day: 'Jueves', orders: 12 },
+      { day: 'Viernes', orders: 7 },
+    ];
+
+     // Configuración de los ejes
+     this.xAxis = {
+      dataField: 'day',
+      displayText: 'Días',
+    };
+
+    this.valueAxis = {
+      unitInterval: 2,
+      minValue: 0,
+      maxValue: 15,
+      displayValueAxis: true,
+      description: 'Órdenes procesadas',
+    };
+
+    // Configuración de los grupos de series
+    this.seriesGroups = [
+      {
+        type: 'column',
+        series: [
+          {
+            dataField: 'orders',
+            displayText: 'Órdenes',
+          },
+        ],
+      },
+    ];
+
+  }  
 
 }
